2f0bc4b1eb6efaa2f550dbc91bf4bc4ddcd10c71
e <1061210163@qq.com>
2020-03-28 18:52:34 +0800

    KMP算法

---
+++ DATAStructure/src/main/java/com/atguigu/kmp/KmpAlgorithm.java
@@ -0,0 +1,45 @@
+package com.atguigu.kmp;
+
+import java.util.Arrays;
+
+public class KmpAlgorithm {
+    public static void main(String[] args) {
+        String str1 = "BBC ABCDAB ABCDABCDABDE";
+        String str2 = "ABCDABD";
+        int[] next = kmpNext(str2);
+        int index = kmpSearch(str1, str2, next);
+        System.out.println("index=" + index);
+    }
+
+    public static int kmpSearch(String str1, String str2, int[] next) {
+        for (int i = 0, j = 0; i < str1.length(); i++) {
+            while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (str1.charAt(i) == str2.charAt(j)) {
+                j++;
+            }
+            if (j == str2.length()) {
+                return i - (j - 1);
+            }
+        }
+        return -1;
+    }
+
+    //获取到一个字符串(子串)的部分匹配值表
+    public static int[] kmpNext(String dest) {
+        //创建一个next数组 把偶才能部分匹配值
+        int[] next = new int[dest.length()];
+        next[0] = 0;
+        for (int i = 1, j = 0; i < dest.length(); i++) {
+            while (j > 0 && dest.charAt(i) != dest.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (dest.charAt(i) == dest.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
+    }
+}
