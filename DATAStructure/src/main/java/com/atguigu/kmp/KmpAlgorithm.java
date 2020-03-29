40b2b40d10cae056cc4ac8330c7da80541af053c
e <1061210163@qq.com>
2020-03-29 09:03:24 +0800

    KMP算法 两种写法

--- DATAStructure/src/main/java/com/atguigu/kmp/KmpAlgorithm.java
+++ DATAStructure/src/main/java/com/atguigu/kmp/KmpAlgorithm.java
@@ -5,19 +5,29 @@ import java.util.Arrays;
 public class KmpAlgorithm {
     public static void main(String[] args) {
         String str1 = "尚硅谷你 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
-        String str2 = "尚硅谷你尚硅";
+        String str2 = "谷你尚";//尚硅谷你尚硅
         int[] next = kmpNext(str2);
         int index = kmpSearch(str1, str2, next);
         System.out.println("index=" + index);
         System.out.println(count);
     }
-    static  int count=0;
+
+    static int count = 0;
 
     public static int kmpSearch(String str1, String str2, int[] next) {
         for (int i = 0, j = 0; i < str1.length(); i++) {
             count++;
             while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
-                j = next[j - 1];
+                //i=i-j+1复原后右移j-next[j-1] <=> j左移j-(j-next[j-1])=next[j-1] 所以有两种写法 这两种效果相同 但是第2种不好理解
+                //#region 1 i要复原右移 j要置0
+                i = i - j + 1 + (j - next[j - 1]);
+                j = 0;
+                //endregion
+
+                //#region 2 i可以保持 只需要j左移
+                //j = next[j - 1];
+                //endregion
+
 
             }
             if (str1.charAt(i) == str2.charAt(j)) {
