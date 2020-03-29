6927545208b3144daa18b062e7aa9fbaecf8a143
e <1061210163@qq.com>
2020-03-29 18:36:38 +0800

    11

--- DATAStructure/src/main/java/com/atguigu/dijkstra/DijkstraAlgorithm.java
+++ DATAStructure/src/main/java/com/atguigu/dijkstra/DijkstraAlgorithm.java
@@ -20,6 +20,7 @@ public class DijkstraAlgorithm {
         martrix[6] = new int[]{2, 3, N, N, 4, 6, N};
         Graph graph = new Graph(vertex, martrix);
         graph.showGraph();
+        graph.dsj(6);
 
     }
 }
@@ -27,6 +28,7 @@ public class DijkstraAlgorithm {
 class Graph {
     private char[] vertex;//顶点数组
     private int[][] martrix;//邻接矩阵
+    private VisitedVertex vv;//已经访问的顶点的集合
 
     //构造器
     public Graph(char[] vertex, int[][] martrix) {
@@ -41,4 +43,104 @@ class Graph {
         }
     }
 
+    /**
+     * 迪杰斯特拉算法实现
+     *
+     * @param index 表示出发顶点对应的下标
+     */
+    public void dsj(int index) {
+        vv = new VisitedVertex(this.vertex.length, index);
+        //更新index顶点到周围顶点的距离
+        update(index);
+    }
+
+    //更新index下标顶点到周围顶点的距离和周围顶点的前驱顶点
+    private void update(int index) {
+        int len = 0;
+        //根据遍历我们的邻接矩阵的martrix{index]行
+        for (int j = 0; j < martrix[index].length; j++) {
+            //len 含义是: 出发顶点到index顶点的距离+从index顶点到j的距离的和
+            len = vv.getDis(index) + martrix[index][j];
+            //如果j顶点没有被访问过 并且len 小于出发顶点到j顶点的距离 就需要更新
+            if (!vv.in(j) && len < vv.getDis(j)) {
+                //出发-》index-》j
+                vv.updatePre(j, index);//j顶点的前驱更新为index顶点
+                vv.updateDis(j, len);//更新出发顶点到j顶点的距离
+            }
+        }
+    }
+
+}
+
+class VisitedVertex {
+    //记录各个顶点时是否访问过 1表示访问过 0问访问 会动态更新
+    public int[] already_arr;
+    //每个下标对应的值为前一个顶点下标 会动态更新
+    public int[] pre_visited;
+    //记录出发顶点到其他所有顶点的距离
+    // 比如G为出发顶点 就会记录G到其他顶点的距离 会动态更新
+    //求的最短距离就会存放到dis
+    public int[] dis;
+
+    /**
+     * @param length 表示顶点的个数
+     * @param index  出发顶点对应的下标比如G顶点 下标就为6
+     */
+    public VisitedVertex(int length, int index) {
+        this.already_arr = new int[length];
+        this.pre_visited = new int[length];
+        this.dis = new int[length];
+        //初始化 dis数组
+        Arrays.fill(dis, 65535);
+        this.already_arr[index] = 1;
+        //设置出发顶点的访问距离为0
+        this.dis[index] = 0;
+    }
+
+    /**
+     * 功能: 判断index顶点是否被访问过
+     *
+     * @param index
+     * @return 访问过:true 未访问:fasle
+     */
+    public boolean in(int index) {
+        return already_arr[index] == 1;
+    }
+
+    /**
+     * 功能: 更新出发顶点到index顶点的距离
+     *
+     * @param index
+     * @param len
+     */
+    public void updateDis(int index, int len) {
+        dis[index] = len;
+    }
+
+    /**
+     * 功能: 更新pre这个顶点的前驱顶点为index节点
+     *
+     * @param pre
+     * @param index
+     */
+    public void updatePre(int pre, int index) {
+        pre_visited[pre] = index;
+    }
+
+    /**
+     * 功能: 返回出发顶点到index这个顶点的距离
+     *
+     * @param index
+     */
+    public int getDis(int index) {
+        return dis[index];
+    }
+
+    //继续选择并返回新的访问顶点 比如这里G完后 就是A作为新的访问顶点(注意不是出发点)
+    public void updateArr() {
+        int min = 65535, index = 0;
+        for (int i = 0; i < already_arr.length; i++) {
+
+        }
+    }
 }
