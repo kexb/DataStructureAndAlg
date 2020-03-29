958a5d39fda60d6906e07e52a399329cd0353954
e <1061210163@qq.com>
2020-03-29 09:37:48 +0800

    1

--- DATAStructure/src/main/java/com/atguigu/kmp/KmpAlgorithm.java
+++ DATAStructure/src/main/java/com/atguigu/kmp/KmpAlgorithm.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 public class KmpAlgorithm {
     public static void main(String[] args) {
-        String str1 = "尚硅谷你 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
-        String str2 = "谷你尚";//尚硅谷你尚硅
+//        String str1 = "尚硅谷你 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
+//        String str2 = "谷你尚";//尚硅谷你尚硅
+
+        String str1 = "ABCDABCDEFG";
+        String str2 = "ABCDEFG";//尚硅谷你尚硅
         int[] next = kmpNext(str2);
         int index = kmpSearch(str1, str2, next);
         System.out.println("index=" + index);
@@ -19,12 +22,16 @@ public class KmpAlgorithm {
             count++;
             while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
                 //i=i-j+1复原后右移j-next[j-1] <=> j左移j-(j-next[j-1])=next[j-1] 所以有两种写法 这两种效果相同 但是第2种不好理解
-                //#region 1 i要复原右移 j要置0
-                i = i - j + 1 + (j - next[j - 1]);
+                //#region 1
+                // 1.1 (i-j) 复原
+                // 1.2 j-next[j-1] 移多少位
+                i = i - j  + (j  - next[j - 1]);
                 j = 0;
                 //endregion
 
                 //#region 2 i可以保持 只需要j左移
+                // 2.1 位移 k=j-next[j-1]位
+                // 2.2 j=j-k=j-(j-next[j-1])=j-j+next[j-1]=next[j-1]
                 //j = next[j - 1];
                 //endregion
 
