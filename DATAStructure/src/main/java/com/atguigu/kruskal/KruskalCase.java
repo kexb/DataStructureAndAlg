1beae52cdcb75fcb6e82ad5c73b766900876f80f
e <1061210163@qq.com>
2020-03-29 15:25:05 +0800

    1

--- DATAStructure/src/main/java/com/atguigu/kruskal/KruskalCase.java
+++ DATAStructure/src/main/java/com/atguigu/kruskal/KruskalCase.java
@@ -1,5 +1,7 @@
 package com.atguigu.kruskal;
 
+import java.util.Arrays;
+
 //克鲁斯卡尔案例
 public class KruskalCase {
     private int edgeNum;//边的个数
@@ -24,6 +26,13 @@ public class KruskalCase {
         kruskalCase.print();
     }
 
+    private static void testSort(KruskalCase kruskalCase) {
+        EData[] edges = kruskalCase.getEdges();
+        System.out.println("排序前=" + Arrays.toString(edges));
+        kruskalCase.sortEdges(edges);
+        System.out.println("排序后=" + Arrays.toString(edges));
+    }
+
     ;
 
     public KruskalCase(char[] vertexs, int[][] martrix) {
@@ -43,7 +52,7 @@ public class KruskalCase {
         }
         //统计边
         for (int i = 0; i < vlen; i++) {
-            for (int j = 0; j < vlen; j++) {
+            for (int j = i + 1; j < vlen; j++) {
                 if (this.matrix[i][j] != INF) {
                     this.edgeNum++;
                 }
@@ -60,5 +69,89 @@ public class KruskalCase {
             }
             System.out.println();
         }
+    }
+
+    //对边进行排序处理,冒泡排序
+    public void sortEdges(EData[] edges) {
+        for (int i = 0; i < edges.length - 1; i++) {
+            for (int j = 0; j < edges.length - i - 1; j++) {
+                if (edges[j].weight > edges[j + 1].weight) {
+                    EData tmp = edges[j];
+                    edges[j] = edges[j + 1];
+                    edges[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param ch 顶点的值 比如'A','B'
+     * @return 返回ch顶点对应的下标 如果找不到返回-1
+     */
+    public int getPosition(char ch) {
+        for (int i = 0; i < vertexts.length; i++) {
+            if (vertexts[i] == ch) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 功能: 获取途中边 放到EData[]数组 后面我们需要遍历该数组
+     * 是通过martrix 邻接矩阵来获取
+     * EData[] 形式[['A','B',12],['B'],'F',7]......]
+     *
+     * @return
+     */
+    private EData[] getEdges() {
+        int index = 0;
+        EData[] edges = new EData[this.edgeNum];
+        for (int i = 0; i < vertexts.length; i++) {
+            for (int j = i + 1; j < vertexts.length; j++) {
+                if (this.matrix[i][j] != INF) {
+                    edges[index++] = new EData(this.vertexts[i], this.vertexts[j], this.matrix[i][j]);
+                }
+            }
+        }
+        return edges;
+    }
+
+    /**
+     * 功能: 获取下标为i的顶点的终点 用于后面判断两个顶点的终点是否相同
+     *
+     * @param ends 数组就是记录了各个顶点对应的终点是哪个 ends数组是在遍历过程中 逐步形成的
+     * @param i    传入顶点对应的下标
+     * @return 返回的就是下标为i的这个顶点对应的终点的下标
+     */
+    private int getEnd(int[] ends, int i) {
+        while (ends[i] != 0) {
+            i = ends[i];
+        }
+        return i;
+    }
+
+}
+
+//编写一个雷EData 他的对象实例就表示一条边
+class EData {
+    char start;//边的一个点
+    char end;//边的另外一个点
+    int weight;//边的权值
+
+    public EData(char start, char end, int weight) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+    //重写toString 便于输出
+
+    @Override
+    public String toString() {
+        return "EData{" +
+                "start=" + start +
+                ", end=" + end +
+                ", weight=" + weight +
+                '}';
     }
 }
