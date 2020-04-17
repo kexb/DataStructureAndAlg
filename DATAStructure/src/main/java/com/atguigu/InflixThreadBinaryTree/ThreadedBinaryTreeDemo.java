124d2c1251d990af2196e256d025890f4a16e64c
e <1061210163@qq.com>
2020-04-17 18:26:10 +0800

    中序线索二叉树位置移动

---
+++ DATAStructure/src/main/java/com/atguigu/InflixThreadBinaryTree/ThreadedBinaryTreeDemo.java
@@ -0,0 +1,335 @@
+package com.atguigu.InflixThreadBinaryTree;
+
+public class ThreadedBinaryTreeDemo {
+    public static void main(String[] args) {
+        HeroNode root = new HeroNode(1, "tom");
+        HeroNode node2 = new HeroNode(3, "jack");
+        HeroNode node3 = new HeroNode(6, "smith");
+        HeroNode node4 = new HeroNode(8, "mary");
+        HeroNode node5 = new HeroNode(10, "king");
+        HeroNode node6 = new HeroNode(14, "dim");
+
+        root.setLeft(node2);
+        root.setRight(node3);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node3.setLeft(node6);
+        System.out.println("找到线索吧");
+
+        ThreadedBinaryTree threadedBinaryTree = new ThreadedBinaryTree();
+        threadedBinaryTree.setRoot(root);
+        threadedBinaryTree.threadedNodes();
+
+        //测试
+        HeroNode leftNode = node5.getLeft();
+        HeroNode rightNode = node5.getRight();
+        System.out.println("10号节点的前驱节点是=" + leftNode);
+        System.out.println("10号节点的后继节点是=" + rightNode);
+
+        System.out.println("===============");
+        threadedBinaryTree.threadedList();
+    }
+}
+
+//实现线索化功能的二叉树
+class ThreadedBinaryTree {
+    private HeroNode root;
+    //为了实现线索化,需要创建一个指向当前节点的前驱节点的指针
+    //在递归进行线索化时,pre总是保留前一个节点
+    private HeroNode pre = null;
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+
+    //遍历线索化二叉树的方法
+    public void threadedList() {
+        //                1
+        //
+        //      3                      6
+        //
+        // 8         10           14
+        //定义一个变量,存储当前遍历的节点,从root开始
+        HeroNode node = root;
+        while (node != null) {
+            //循环的找到leftType==1的节点，第一个找到的就是8节点
+            //后面随着遍历而变化,因为当leftType==1时，说明该节点是按照线索化
+            //处理后的有效节点
+            while (node.getLeftType() == 0) {
+                node = node.getLeft();
+            }
+            System.out.println(node);
+            //打印当前节点的右指针指向的是后继节点，就一直输出  树形如果是一路向左 那么右子树都是空的 可以做右线索
+            while (node.getRightType() == 1) {
+                node = node.getRight();
+                System.out.println(node);
+            }
+            //替换这个遍历的节点
+            node = node.getRight();
+
+
+        }
+    }
+
+    public void threadedNodes() {
+        threadedNodes(this.root);
+    }
+
+    //编写对二叉树进行中序线索化的方法
+
+    /**
+     * @param node node就是当前需要线索化的节点
+     */
+    public void threadedNodes(HeroNode node) {
+        if (node == null) {
+            return;
+        }
+        //(一)先线索化左子树
+        threadedNodes(node.getLeft());
+        //(二)线索化当前节点
+
+        //处理当前节点的前驱节点
+        //以8来理解.left=null , 8.leftType=1
+        if (node.getLeft() == null) {
+            //让当前节点的左指针指向前驱节点
+            node.setLeft(pre);
+            //修改当前节点的左指针类型
+            node.setLeftType(1);
+        }
+        //处理后继节点
+        if (this.pre != null && this.pre.getRight() == null) {
+            //让前驱节点的右指针指向当前节点
+            this.pre.setRight(node);
+            //修改前驱节点的右指针类型
+            this.pre.setRightType(1);
+        }
+        pre = node;
+
+        //(三)再线索化右子树
+        threadedNodes(node.getRight());
+    }
+
+    public void delNode(int no) {
+        if (root != null) {
+            if (root.getNo() == no) {
+                root = null;
+            } else {
+                //递归删除
+                root.delNode(no);
+            }
+        } else {
+            System.out.println("空树不能删除");
+        }
+    }
+
+    //前续遍历
+    public HeroNode preOrderSearch(int no) {
+        if (root != null) {
+            return this.root.preOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    //中续遍历
+    public HeroNode inflixOrderSearch(int no) {
+        if (root != null) {
+            return this.root.infixOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    //后续遍历
+    public HeroNode postOrderSearch(int no) {
+        if (root != null) {
+            return this.root.postOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+}
+
+class HeroNode {
+    //leftType:0 指向左子树 leftType=1 前驱节点
+    private int leftType;
+    //rightType:0 指右子树 leftType=1 后继节点
+    private int rightType;
+
+    public HeroNode(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HeroNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HeroNode left) {
+        this.left = left;
+    }
+
+    public HeroNode getRight() {
+        return right;
+    }
+
+    public void setRight(HeroNode right) {
+        this.right = right;
+    }
+
+    private int no;
+    private String name;
+    private HeroNode left;
+    private HeroNode right;
+
+    private HeroNode(int no) {
+        this.no = no;
+        this.name = name;
+    }
+
+    public void setLeftType(int leftType) {
+        this.leftType = leftType;
+    }
+
+    public void setRightType(int rightType) {
+        this.rightType = rightType;
+    }
+
+    public int getLeftType() {
+        return leftType;
+    }
+
+    public int getRightType() {
+        return rightType;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void delNode(int no) {
+        if (this.left != null && this.left.getNo() == no) {
+            this.left = null;
+            return;
+        }
+        if (this.right != null && this.right.getNo() == no) {
+            this.right = null;
+            return;
+        }
+        if (this.left != null) {
+            this.left.delNode(no);
+        }
+        if (this.right != null) {
+            this.right.delNode(no);
+        }
+    }
+
+    public void preOrder() {
+        System.out.println(this);
+        if (this.left != null) {
+            this.left.preOrder();
+        }
+        if (this.right != null) {
+            this.right.preOrder();
+        }
+    }
+
+    public void infixOrder() {
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+
+    public void postOrder() {
+        if (this.left != null) {
+            this.left.postOrder();
+        }
+        if (this.right != null) {
+            this.right.postOrder();
+        }
+        System.out.println(this);
+    }
+
+    public HeroNode preOrderSearch(int no) {
+        System.out.println("进入前序遍历");
+        if (this.no == no) {
+            return this;
+        }
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.preOrderSearch(no);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.right != null) {
+            resNode = this.right.preOrderSearch(no);
+        }
+        return resNode;
+
+    }
+
+    public HeroNode infixOrderSearch(int no) {
+        System.out.println("进入前序遍历");
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.infixOrderSearch(no);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.no == no) {
+            return this;
+        }
+        if (this.right != null) {
+            resNode = this.right.infixOrderSearch(no);
+        }
+        return resNode;
+
+    }
+
+    public HeroNode postOrderSearch(int no) {
+        System.out.println("进入前序遍历");
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.postOrderSearch(no);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.right != null) {
+            resNode = this.right.postOrderSearch(no);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+        if (this.no == no) {
+            return this;
+        }
+        return resNode;
+    }
+}
