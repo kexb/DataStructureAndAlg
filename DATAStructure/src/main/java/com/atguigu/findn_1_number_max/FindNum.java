d5804ee6692d31fda8b66510d40e5177015207ff
e <1061210163@qq.com>
2020-05-08 10:50:34 +0800

    动态规划算法N个数中 选择N-1个数 使得乘积最大

---
+++ DATAStructure/src/main/java/com/atguigu/findn_1_number_max/FindNum.java
@@ -0,0 +1,38 @@
+package com.atguigu.findn_1_number_max;
+
+import java.util.Scanner;
+
+public class FindNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("你要测试多少个数的乘积：");
+        int n=sc.nextInt();
+        int[] num=new int[n];
+        for (int i = 0; i <n ; i++) {
+            System.out.println("请输入第"+(i+1)+"个数：");
+            num[i]=sc.nextInt();
+        }
+        int[] dp=new int[n+1];//dp表示长度为n的数组中选取n-1个数字的最大乘积
+        int[] min=new int[n+1];//min表示长度为n的数组中选取n-1个数字的最小乘积
+        dp[0]=0;
+        dp[1]=0;//长度为1时，最大乘积为0
+        dp[2]=Math.max(num[0],num[1]);//长度为2时，最大乘积为两个数中的较大值
+        min[0]=0;
+        min[1]=0;
+        min[2]=Math.min(num[0],num[1]);//长度为2时，最小乘积为两个数中的较小值
+        for (int i = 3; i <=n ; i++) {
+            dp[i]=Math.max(getResult(num,i-1),Math.max(dp[i-1]*num[i-1],min[i-1]*num[i-1]));
+            min[i]=Math.min(getResult(num,i-1),Math.min(dp[i-1]*num[i-1],min[i-1]*num[i-1]));
+        }
+        System.out.println(dp[n]);
+    }
+    //求数组乘积
+    public static int getResult(int[] num,int i){
+        int result=1;
+        for (int j = 0; j <i ; j++) {
+            result=result*num[j];
+        }
+        return result;
+    }
+
+}
