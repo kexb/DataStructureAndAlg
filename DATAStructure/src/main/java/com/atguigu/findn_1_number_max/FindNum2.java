6b84d1b2158b714a2b9a5157683077197ea685a1
e <1061210163@qq.com>
2020-05-08 15:52:51 +0800

    1

--- DATAStructure/src/main/java/com/atguigu/findn_1_number_max/FindNum2.java
+++ DATAStructure/src/main/java/com/atguigu/findn_1_number_max/FindNum2.java
@@ -6,13 +6,7 @@ public class FindNum2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("你要测试多少个数的乘积：");
-//        int n=sc.nextInt();
-//        int[] num=new int[n];
-//        for (int i = 0; i <n ; i++) {
-//            System.out.println("请输入第"+(i+1)+"个数：");
-//            num[i]=sc.nextInt();
-//        }
-        int[] num = {4, 5, 1, 6, 3, 10};
+        int[] num = {5, 6, 3, 9, 10, 7};
         int n = num.length;
         int[] dp = new int[n + 1];//dp表示长度为n的数组中选取n-1个数字的最大乘积
         int[] min = new int[n + 1];//min表示长度为n的数组中选取n-1个数字的最小乘积
@@ -23,6 +17,7 @@ public class FindNum2 {
         min[1] = 0;
         min[2] = Math.min(num[0], num[1]);//长度为2时，最小乘积为两个数中的较小值
         List<Integer> path = new ArrayList<>();
+        List<Integer> root = new ArrayList<>();
         //选出i-1个数乘积最大
         for (int i = 3; i <= n; i++) {
             int commonResult = getResult(num, i - 1);
@@ -30,11 +25,11 @@ public class FindNum2 {
             int maxResult = Math.max(dp[i - 1] * next, min[i - 1] * next);
             int minResult = Math.min(dp[i - 1] * next, min[i - 1] * next);
             min[i] = Math.min(commonResult, minResult);//Math.min(dp[i-1]*num[i-1],min[i-1]*num[i-1]) 参数1可以变为最小值，参数2也可能是最大值
-            if (commonResult > maxResult) {
+            if (commonResult > maxResult && commonResult > minResult) {
                 dp[i] = commonResult;
                 path.clear();
                 for (int k = 0; k < (i - 1); k++) {
-                    path.add(num[k]);
+                    path.add(k);
                 }
             } else {
                 if (maxResult > minResult) {
@@ -42,12 +37,25 @@ public class FindNum2 {
                 } else {
                     dp[i] = minResult;
                 }
-                path.add(num[i - 1]);
+                path.add(i - 1);
             }
 
 
         }
-        System.out.println(path);
+        if (num[0] > num[1]) {
+            if (!path.contains(0)) {
+                path.add(0, 0);
+            }
+        } else {
+            if (!path.contains(1)) {
+                path.add(0, 1);
+            }
+
+        }
+        for (int i = 0; i < path.size(); i++) {
+            root.add(i, num[path.get(i)]);
+        }
+        System.out.println(root);
         System.out.println(dp[n]);
     }
 
