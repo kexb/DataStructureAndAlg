9a9bfee568594b5fbf281d5d428dee5b199c48ec
e <1061210163@qq.com>
2020-05-09 23:02:52 +0800

    1

--- DATAStructure/src/main/java/com/atguigu/rbtree/testRedBlackTreeDemo.java
+++ DATAStructure/src/main/java/com/atguigu/rbtree/testRedBlackTreeDemo.java
@@ -2,12 +2,5 @@ package com.atguigu.rbtree;
 
 public class testRedBlackTreeDemo {
     public static void main(String[] args) {
-        RBTree<Integer> rbTree=new RBTree<>();
-        for (int i = 1; i <= 10; i++) {
-            rbTree.addNode(i);
-        }
-        RBTreeNode<Integer> root = rbTree.getRoot();
-        rbTree.printTree(root);
     }
-
 }
